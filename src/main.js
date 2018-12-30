fe38ed6a3c6df5b674b77f4ba5b275b1e8d33166
Axel Greavette <vultureavatar@gmail.com>
2018-12-30 15:48:34 -0500

    Added tray menu functionality

--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@
 
 /* eslint-disable no-console */
 
-const { app, BrowserWindow, nativeImage, ipcMain, globalShortcut } = require('electron');
+const { app, Tray, Menu, BrowserWindow, nativeImage, ipcMain, globalShortcut } = require('electron');
 const path = require('path');
 const url = require('url');
 const rpc = require('discord-rich-presence')('528735337015410712');
-
 const AppUpdater = require('./modules/AppUpdater');
 
 const startTimestamp = new Date();
@@ -27,6 +26,7 @@ const updater = new AppUpdater('info');
 let endUpdateData = null;
 
 let mainWindow;
+let tray = null;
 
 const createWindow = () => {
 	mainWindow = new BrowserWindow({
@@ -45,8 +45,43 @@ const createWindow = () => {
 	const icon = nativeImage.createFromPath(path.join(__dirname, 'assets/logo_square_512.png'));
 	mainWindow.setIcon(icon);
 
-	mainWindow.on('closed', () => {
-		mainWindow = null;
+	mainWindow.on('close', event => {
+		event.preventDefault();
+		mainWindow.hide();
+	});
+
+	tray = new Tray(icon);
+
+	const contextMenu = Menu.buildFromTemplate([
+		{
+			label: 'Support Server',
+			click() {
+				require('electron').shell.openExternal('https://discord.gg/xna9NRh');
+			},
+			icon: nativeImage.createFromPath(path.join(__dirname, 'assets/Discord-Logo-Black.png')).resize({width: 18, height: 18, quality: 'best'})
+		},
+		{
+			label: 'Source Code',
+			click() {
+				require('electron').shell.openExternal('https://github.com/RailRunner166/MyRPC');
+			},
+			icon: nativeImage.createFromPath(path.join(__dirname, 'assets/Github-Logo-Black.png')).resize({width: 18, height: 18, quality: 'best'})
+		},
+		{
+			label: 'Exit MyRPC',
+			click() { 
+				app.exit()
+			},
+			icon: nativeImage.createFromPath(path.join(__dirname, 'assets/Close-Me-Button-Black.png')).resize({width: 18, height: 18, quality: 'best'})
+		}	
+	]);
+
+	tray.setToolTip('MyRPC');
+
+	tray.setContextMenu(contextMenu);
+
+	tray.on('click', () => {
+		mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
 	});
 }
 
@@ -71,6 +106,7 @@ app.on('ready', () => {
 	setActivity(rpcData);
 });
 
+
 app.on('window-all-closed', () => {
 	app.quit();
 });
@@ -89,7 +125,8 @@ ipcMain.on('asynchronous-message', (e, data) => {
 	rpcData.largeImageKey = data.largeImageKey;
 	rpcData.smallImageKey = data.smallImageKey;
 
-	console.debug(rpcData);
+	//the fuck is this? debug mode isn't on!
+	//console.debug(rpcData);
 
 	setActivity(rpcData);
 })
