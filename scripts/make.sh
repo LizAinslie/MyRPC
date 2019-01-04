8bc4a660966a479c9039de4c2e64fbb03b601584
Landon Gravat <railinator4903@gmail.com>
2019-01-03 21:08:42 -0600

    oof syntax error

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,6 +1,6 @@
 if [[ $TRAVIS_OS_NAME = "osx" && -z $TRAVIS_TAG ]]; then
-    npm run make:osx;
+    npm run make:osx
 fi
 if [[ $TRAVIS_OS_NAME = "linux" && -z $TRAVIS_TAG ]]; then
-    npm run make;
+    npm run make
 fi
