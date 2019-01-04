55987f1e194701a6a2aa64e526baf4381158872a
Landon Gravat <railinator4903@gmail.com>
2019-01-03 21:49:03 -0600

    fixxxxxxx

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,9 +1,11 @@
-if [ $TRAVIS_OS_NAME = "osx" && -z $TRAVIS_TAG ] 
+if [[ -z $TRAVIS_TAG ]] 
 then
-    npm run makeosx
-fi
-
-if [ $TRAVIS_OS_NAME = "linux" && -z $TRAVIS_TAG ] 
-then
-    npm run make
+    if [[ $TRAVIS_OS_NAME = "osx" ]] 
+    then
+        npm run makeosx
+        # echo "osx"
+    else
+        npm run make
+        # echo "linux"
+    fi
 fi
