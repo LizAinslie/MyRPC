eecd09b96cfb30a34ce8ea1bb3aa565ec25dbd69
Landon Gravat <railinator4903@gmail.com>
2019-01-03 21:24:32 -0600

    work pls kthx

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,6 +1,7 @@
-if [[ $TRAVIS_OS_NAME = "osx" && -z $TRAVIS_TAG ]]; then
-    npm run make:osx
+if [ $TRAVIS_OS_NAME = "osx" && -z $TRAVIS_TAG ]; then
+    npm run makeosx
 fi
-if [[ $TRAVIS_OS_NAME = "linux" && -z $TRAVIS_TAG ]]; then
+
+if [ $TRAVIS_OS_NAME = "linux" && -z $TRAVIS_TAG ]; then
     npm run make
 fi
