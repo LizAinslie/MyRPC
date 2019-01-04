c5723d6e4b0217fdd9fade8321945b5b8fbdf588
Landon Gravat <railinator4903@gmail.com>
2019-01-03 21:33:16 -0600

    bitchy bash script

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,7 +1,9 @@
-if [ $TRAVIS_OS_NAME = "osx" && -z $TRAVIS_TAG ]; then
+if [ $TRAVIS_OS_NAME = "osx" && -z $TRAVIS_TAG ] 
+then
     npm run makeosx
 fi
 
-if [ $TRAVIS_OS_NAME = "linux" && -z $TRAVIS_TAG ]; then
+if [ $TRAVIS_OS_NAME = "linux" && -z $TRAVIS_TAG ] 
+then
     npm run make
 fi
