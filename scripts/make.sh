38086d9e435dd59e560bda89154f063f4cb86655
Landon Gravat <railinator4903@gmail.com>
2019-01-04 20:37:01 -0600

    custom debian build script

--- scripts/make.sh
+++ scripts/make.sh
@@ -5,10 +5,35 @@ if [ -z "$TRAVIS_TAG" ]
 then
 	if [ "$TRAVIS_OS_NAME" == "osx" ] 
 	then
-		echo "osx"
+		echo "PLATFORM: osx"
 		npm run makeosx
 	else
-		echo "linux"
+		echo "PLATFORM: linux"
 		npm run make
+		mkdir MyRPC_deb
+		cd MyRPC_deb
+		mkdir usr && mkdir DEBIAN
+		cd usr && mkdir share && cd share
+		mkdir applications && cd applications
+		echo "[Desktop Entry]
+Version=$TRAVIS_TAG
+Name=MyRPC
+Comment=Easily set your Discord status to anything!
+Exec=/usr/share/myrpc/MyRPC
+Path=/usr/share/myrpc/
+Icon=/usr/share/myrpc/resources/app/src/assets/icons/512x512.png
+Terminal=false
+Type=Application
+Categories=Utility;" > myrpc.desktop
+		cd ../../../DEBIAN
+		echo "Package: myrpc
+Maintainer: Kyle Smith <kyleafmine [at] gmail.com>
+Architecture: amd64
+Version: $TRAVIS_TAG
+Description: Easily set your Discord status to anything!" > control
+		cd ../..
+		cp -r out/MyRPC-linux-x64 MyRPC_deb/usr/share/myrpc
+		dpkg -b MyRPC_deb
+		mv MyRPC_deb.deb MyRPC_v$($TRAVIS_TAG)_x64.deb
 	fi
 fi
